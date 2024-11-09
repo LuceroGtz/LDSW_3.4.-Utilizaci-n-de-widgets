b7b01e52166a2422b1dcd9dc437127cf28549628
Lucero Gtz <gtzroblucero@gmail.com>
2024-11-09 14:22:20 -0600

    actividad 3.6 peticiones HTTP

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,7 +24,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String marvelCharacterName = '';
+  String marvelCharacterImageUrl = '';
+
+  // Función para obtener datos de un personaje de Marvel
+  Future<void> fetchMarvelCharacterData() async {
+    final publicKey = 'cfa6bd6b0ec3274c030a7aaba41248d2';  
+    final privateKey = '4d8506c0ac58bb85081e590b72ba9a049553efd7';  
+    final timestamp = DateTime.now().millisecondsSinceEpoch.toString();
+    final hash = md5.convert(utf8.encode('$timestamp$privateKey$publicKey')).toString();
+    final randomCharacterId = 1009148; 
+
+    final url = Uri.parse(
+      'https://gateway.marvel.com/v1/public/characters/$randomCharacterId?ts=$timestamp&apikey=$publicKey&hash=$hash',
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body)['data']['results'][0];
+      setState(() {
+        marvelCharacterName = data['name'];
+        marvelCharacterImageUrl = '${data['thumbnail']['path']}.${data['thumbnail']['extension']}';
+      });
+    } else {
+      throw Exception('Error al cargar los datos de Marvel');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -35,12 +71,12 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
         ),
-        // Contenido
+        
         Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Mensaje de bienvenida en tres renglones
+              
               Text(
                 'Bienvenido\nal club\nde la película',
                 style: TextStyle(
@@ -55,7 +91,7 @@ class MyHomePage extends StatelessWidget {
               // Botón para entrar
               ElevatedButton(
                 onPressed: () {
-                  // Acción del botón
+                 
                 },
                 child: Text('Entrar'),
                 style: ElevatedButton.styleFrom(
@@ -63,6 +99,28 @@ class MyHomePage extends StatelessWidget {
                   textStyle: TextStyle(fontSize: 18),
                 ),
               ),
+              SizedBox(height: 50),
+              // Botón para ver personaje de Marvel
+              ElevatedButton(
+                onPressed: fetchMarvelCharacterData,
+                child: Text('Ver Personaje de Marvel'),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  textStyle: TextStyle(fontSize: 18),
+                ),
+              ),
+              // Mostrar los datos del personaje
+              marvelCharacterName.isNotEmpty
+                  ? Column(
+                      children: [
+                        Text(
+                          'Marvel: $marvelCharacterName',
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Image.network(marvelCharacterImageUrl),
+                      ],
+                    )
+                  : Container(),
             ],
           ),
         ),
@@ -70,4 +128,3 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-
